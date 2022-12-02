b564f553f09c68be0567fce7fe8caddb78eef638
BaekDoHyung <qorehgud@cnrres.com>
2022-12-02 16:45:45 +0900

    불필요 파일 제거 및 텍스트 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                2023년 2월 4일 오후 5시
+            </div>
+            <div>
+                백도형 ♥ 유선희
+            </div>
+
+            <div>
+                저희 두 사람, 뽀얀 눈송이 같은 순수한 사랑 모아 인생의 반려자가 되려 합니다.
+
+                바쁘신 가운데 찾아주시어 새로운 시작을 축복해 주시면 더 없는 기쁨으로 간직하겠습니다.
+            </div>
+
+            <div>
+                백기운 · 이경미 의 장남 도형
+            </div>
+            <div>
+                유순근 · 강향순 의 장녀 선희
+            </div>
+        </div>
+    );
 }
 
 export default App;
