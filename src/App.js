bd3f666086e87abd97819df08e6ff968122a5a79
BaekDorazi <qorehgud@gmail.com>
2022-12-10 14:32:45 +0900

    글자 크기, 위치, 색 변경작업중

--- src/App.js
+++ src/App.js
@@ -26,47 +26,63 @@ const App = () => {
 
     return (
         <div id="main" className="App">
-            {renderSnow()}
+            <div className="front-area">
+                {renderSnow()}
 
-            {/*슬라이드 사진 영역*/}
-            {/*<img src={mainImg} width="70%" height="auto" alt=""/>*/}
+                {/*슬라이드 사진 영역*/}
+                <img src={mainImg} width="70%" height="auto" alt=""/>
 
-            {/*결혼식 시간 영역*/}
-            <div className={"neonText"}>
-                <div>
-                    백도형, 유선희
-                </div>
-                <div>
-                    DOHYUNG AND SUNHEE
-                </div>
-                <div>
-                    2023년 2월 4일 토요일 pm 5:00
-                </div>
-                <div>
-                    겨울 밤, 결혼합니다.
+                {/*결혼식 시간 영역*/}
+                <div className={"neonText"}>
+                    <div style={{marginTop: "10px", fontSize: "1.7rem"}}>
+                        백 도 형 , 유 선 희
+                    </div>
+
+                    <div style={{marginTop: "10px", fontSize: "1rem"}}>
+                        DOHYUNG AND SUNHEE
+                    </div>
+
+                    <div style={{marginTop: "20px", fontSize: "1.3rem"}}>
+                        2023년 2월 4일 토요일 pm 5:00
+                    </div>
+
+                    <div style={{padding: "10px", fontSize: "1.5rem"}}>
+                        겨울 밤, 결혼합니다.
+                    </div>
                 </div>
             </div>
 
             {/*문구 영역*/}
-            <div>
-                저희 두 사람, 뽀얀 눈송이 같은 순수한 사랑 모아 인생의 반려자가 되려 합니다.
+            <div style={{padding: "15px", fontSize: "1.1rem"}}>
+                <div>
+                    저희 두 사람, 뽀얀 눈송이 같은 순수한 사랑 모아
+                    <br/>인생의 반려자가 되려 합니다.
+                </div>
 
-                바쁘신 가운데 찾아주시어 새로운 시작을 축복해 주시면 더 없는 기쁨으로 간직하겠습니다.
+                <div style={{marginTop: "15px"}}>
+                    바쁘신 가운데 찾아주시어 새로운 시작을
+                    <br/> 축복해 주시면 더 없는 기쁨으로 간직하겠습니다.
+                </div>
+            </div>
+
+            <div style={{marginTop: "10px"}}>
+                <span style={{fontSize: "1.1rem", fontWeight: "700"}}>백기운 · 이경미</span>
+                <span style={{fontSize: "0.8rem"}}> 의 장남</span>
+                <span style={{fontSize: "1.1rem", color: "#1b2735", fontWeight: "700"}}>도형</span>
             </div>
 
             <div>
-                백기운 · 이경미 의 장남 도형
-            </div>
-            <div>
-                유순근 · 강향순 의 장녀 선희
+                <span style={{fontSize: "1.1rem", fontWeight: "700"}}>유순근 · 강향순</span>
+                <span style={{fontSize: "0.8rem"}}> 의 장녀</span>
+                <span style={{fontSize: "1.1rem", color: "#1b2735", fontWeight: "700"}}>선희</span>
             </div>
 
-            <div>
+            <div style={{marginTop: "15px", fontSize: "1.3rem", alignContent: "left"}}>
                 오시는 길
             </div>
 
             <div>
-                <img src={mapImg} width="80%" height="auto" alt=""/>
+                <img src={mapImg} width="90%" height="auto" alt=""/>
             </div>
 
             <div>축하 문구</div>
